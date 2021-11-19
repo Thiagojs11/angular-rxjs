89816dbd10ef3f819d1339c446114529650ccafa
Thiago Jose <tjose@sccalpha.local>
2021-11-19 09:11:30 -0300

    Async pipe

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-import { Acoes } from './modelo/acoes';
 import { AcoesService } from './acoes.service';
 
 @Component({
@@ -9,15 +9,10 @@ import { AcoesService } from './acoes.service';
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent implements OnInit{
+export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes: Acoes;
+  acoes$ = this.acoesService.getAcoes();
 
   constructor(private acoesService: AcoesService) {}
 
-  ngOnInit(): void {
-    this.acoesService.getAcoes().subscribe((acoes) =>{
-      this.acoes = acoes;
-    })
-  }
 }
